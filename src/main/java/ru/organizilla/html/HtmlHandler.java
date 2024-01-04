7ad1e720df1b1fdc6af2ea56b7d06a8a5d890fa6
ik <ikilpikovv@gmail.com>
2024-01-04 23:05:04 +0300

    Убрал лишние импорты.

--- src/main/java/ru/organizilla/html/HtmlHandler.java
+++ src/main/java/ru/organizilla/html/HtmlHandler.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.function.ToDoubleBiFunction;
 
 @Component
 public class HtmlHandler {
